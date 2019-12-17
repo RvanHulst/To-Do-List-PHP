fb81ebd545fab0c3eb47054e093dc1873ff2755c
RvanHulst <99051006@mydavinci.nl>
2019-12-17 09:18:35 +0100

    first commit
    V1.0

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+header('Content-type: text/html; charset=iso-8859-1');  
+// Create connection
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=ToDoList", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+	include "navbar.php";
+
+//opbouwen SQL statement
+//$sql = 'SELECT * FROM ';
+//$stmt = $conn->prepare($sql);
+//$stmt->execute();
+//$result = $stmt->fetchAll();
+
+//var_dump($result);
+
+?>
+<!DOCTYPE html>
+<html lang="nl" dir="ltr">
+  <head>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="main.css">
+    <meta charset="utf-8">
+    
+  </head>
+  <body>
+
+  </body>
+</html>
+ 
